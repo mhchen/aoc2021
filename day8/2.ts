9e63e5161e107e477a667eb4725a3ad5a9e373b5
Mike Chen <mhchen@gmail.com>
2021-12-10 08:27:46 -0800

    Day8 pt 2 almost quit programming

--- day8/2.ts
+++ day8/2.ts
@@ -2,85 +2,89 @@ import { parseInput } from '../utils';
 
 const lines = parseInput();
 
-// const uniqueSegmentCount = 0;
-
-// const segments = new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
-
-// interface PotentialSegments {
-// top: Set<string>;
-// topRight: Set<string>;
-// topLeft: Set<string>;
-// const segmentsToDigits = new Map([
-// bottom: Set<string>;
-// bottomRight: Set<string>;
-// bottomLeft: Set<string>;
-// }
-
-// function createPotentialSegments(): PotentialSegments {
-// return {
-// top: new Set(segments),
-// topRight: new Set(segments),
-// topLeft: new Set(segments),
-// center: new Set(segments),
-// bottom: new Set(segments),
-// bottomRight: new Set(segments),
-// bottomLeft: new Set(segments),
-// };
-// }
-
-// function eliminate(
-// potentialSegments: PotentialSegments,
-// keys: (keyof PotentialSegments)[],
-// letters: string[],
-// ) {
-// for (const key of keys) {
-// for (const letter of letters) {
-// potentialSegments[key].delete(letter);
-// }
-// }
-// }
-
-const segmentsToDigits = new Map([
-  [6, [0, 6, 9]],
-  [2, [1]],
-  [3, [7]],
-  [4, [4]],
-  [5, [2, 3, 5]],
-  [7, [8]],
-]);
-
-const digitsToSegments = new Map<number, number>();
-
-for (const [segments, digits] of segmentsToDigits) {
-  for (const digit of digits) {
-    digitsToSegments.set(digit, segments);
+function containsSegments(outer: string, inner: string) {
+  const outerSet = new Set(outer.split(''));
+  const innerSet = new Set(inner.split(''));
+  for (const char of innerSet) {
+    if (!outerSet.has(char)) {
+      return false;
+    }
   }
+  return true;
 }
 
-// const segmentLengthToEliminatedSegments: Record<
-// number,
-// (keyof PotentialSegments)[]
-// > = {
-// 4: ['top', 'bottomLeft', 'bottom'],
-// 2: ['top', 'topLeft', 'center', 'bottomLeft', 'bottom'],
-// 3: ['topLeft', 'center', 'bottomLeft', 'bottom'],
-// };
-
-function matchDigits(input: string[]) {
-  const inputMap = {
-
-  };
-  const potentialSegments = segmentsToDigits.get(input.length);
-  if (!potentialSegments) {
-    return;
-  }
-  const potentialSegment = potentialSegments.pop();
-  inputMap[input]
-}
+let sum = 0;
 
 for (const line of lines) {
   const [inputTokens, outputTokens] = line.split(' | ');
-  const digitMap = matchDigits(
-    inputTokens.split(' ').map((input) => input.split('').sort().join('')),
+  const transformedInputs = inputTokens
+    .split(' ')
+    .map((input) => input.split('').sort().join(''));
+
+  const transformedOutputs = outputTokens
+    .split(' ')
+    .map((output) => output.split('').sort().join(''));
+
+  const inputsByLength: Record<number, string[]> = {};
+
+  for (const input of transformedInputs) {
+    inputsByLength[input.length] = inputsByLength[input.length] || [];
+    inputsByLength[input.length].push(input);
+  }
+
+  const inputToDigitMap: Record<string, number> = {};
+  const digitToInputMap: Record<number, string> = {};
+
+  /* eslint-disable prefer-destructuring */
+  inputToDigitMap[inputsByLength[2][0]] = 1;
+  digitToInputMap[1] = inputsByLength[2][0];
+  inputToDigitMap[inputsByLength[3][0]] = 7;
+  digitToInputMap[7] = inputsByLength[3][0];
+  inputToDigitMap[inputsByLength[4][0]] = 4;
+  digitToInputMap[4] = inputsByLength[4][0];
+  inputToDigitMap[inputsByLength[7][0]] = 8;
+  digitToInputMap[8] = inputsByLength[7][0];
+  /* eslint-enable prefer-destructuring */
+
+  const sixSegments = inputsByLength[6];
+
+  // if 6 segments and contains 4, it's 9
+  // if 6 segments and contains 7, it's 0
+  // else it's 6
+
+  // if 5 segments and contains 7, it's 3
+  // if 5 segments and contained by 6, it's 5
+  // else it's 2
+
+  while (sixSegments.length) {
+    const input = sixSegments.pop()!;
+    let digit: number;
+    if (containsSegments(input, digitToInputMap[4])) {
+      digit = 9;
+    } else if (containsSegments(input, digitToInputMap[7])) {
+      digit = 0;
+    } else {
+      digit = 6;
+    }
+    inputToDigitMap[input] = digit;
+    digitToInputMap[digit] = input;
+  }
+
+  const fiveSegments = inputsByLength[5];
+  while (fiveSegments.length) {
+    const input = fiveSegments.pop()!;
+    if (containsSegments(input, digitToInputMap[7])) {
+      inputToDigitMap[input] = 3;
+    } else if (containsSegments(digitToInputMap[6], input)) {
+      inputToDigitMap[input] = 5;
+    } else {
+      inputToDigitMap[input] = 2;
+    }
+  }
+
+  sum += Number(
+    transformedOutputs.map((output) => inputToDigitMap[output]).join(''),
   );
 }
+
+console.log(sum);
